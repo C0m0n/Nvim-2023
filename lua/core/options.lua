038082a49b00535c3c90a93a1da30df05af80ea6
David G <115606741+C0m0n@users.noreply.github.com>
2024-01-06 16:24:59 -0600

    test with neotree

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,7 +12,7 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
-vim.opt.hlsearch = false 
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.scrolloff = 4
@@ -27,5 +27,6 @@ vim.opt.showmatch = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.linebreak = true
+vim.opt.wrap = false
 
 
